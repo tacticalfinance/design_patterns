a44649990f303d6cb82b8beaa4e54e2f8eafa33a
Ruby Salton <ruby@tornadofx.co.il>
2024-01-23 13:35:47 +0200

    remove getter and setter functions

--- adapter/original_car.py
+++ adapter/original_car.py
@@ -12,14 +12,6 @@ class IOrigianlCar(ABC):
     def car_color(self, car_color: str) -> None:
         pass
 
-    @abstractmethod
-    def get_car_color(self) -> str:
-        pass
-
-    @abstractmethod
-    def set_car_color(self, car_color: str) -> None:
-        pass
-
 
 class OriginalCar(IOrigianlCar):
     def __init__(self, car_color: str) -> None:
@@ -33,17 +25,7 @@ class OriginalCar(IOrigianlCar):
     def car_color(self, car_color: str) -> None:
         self.__car_color = car_color
 
-    def get_car_color(self) -> str:
-        return self.__car_color
-
-    def set_car_color(self, car_color: str) -> None:
-        self.__car_color = car_color
-
 
 car = OriginalCar(car_color="yellow")
-
 car.car_color = "white"
-car.set_car_color("white")
-
 print(car.car_color)
-print(car.get_car_color())
